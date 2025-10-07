cf144c66a6e489ae9647b24da17311f8acb73768
mgbpapa04 <mgbpapa04@hotmail.co.uk>
2025-10-07 22:48:00 +0000

    tforthree

--- Tpwlean.lean
+++ Tpwlean.lean
@@ -1,2 +1,1 @@
-import Mathlib.tactic
-
+import Mathlib.Tactic
--- lakefile.lean
+++ lakefile.lean
@@ -1,11 +1,13 @@
-laimport Lake
+import Lake
 open Lake DSL
 
 package «tpwlean» where
   -- Settings for the project
 
+-- This 'require' line must come BEFORE the library definition below.
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
 @[default_target]
 lean_lib «Tpwlean» where
   -- The library name
-
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git"
