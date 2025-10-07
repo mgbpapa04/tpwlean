12cca262e99aac69efabe01a9e6970149fcb6b7c
mortisaac <72670968+mortisaac@users.noreply.github.com>
2025-10-07 17:10:27 +0000

    LEAN ATTEMPT

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package «tpwlean» where
+  -- Settings for the project
+
+@[default_target]
+lean_lib «Tpwlean» where
+  -- The library name
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git"
