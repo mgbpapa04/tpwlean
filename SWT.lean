1255439814514c5a7f919a9ace8fc7a9340f2adb
mortisaac <72670968+mortisaac@users.noreply.github.com>
2025-11-05 17:02:56 +0000

    SWT&UAT

---
+++ SWT.lean
@@ -0,0 +1,11 @@
+-- SWT Imports
+import Mathlib.Tactic
+import Mathlib.Topology.Compactness.Compact
+import Mathlib.Topology.ContinuousMap.Basic
+import Mathlib.Topology.ContinuousMap.Compact
+import Mathlib.Topology.DenseEmbedding
+import Mathlib.Algebra.Algebra.Subalgebra.Basic
+import Mathlib.Topology.ContinuousMap.Algebra
+import Mathlib.Topology.Instances.Real.Lemmas
+import Mathlib.Topology.ContinuousMap.Weierstrass
+import Mathlib.Data.Real.Basic
---
+++ UAT.lean
@@ -0,0 +1,5 @@
+-- UAT Imports
+import Mathlib.Tactic
+import Mathlib.Algebra.Module.LinearMap.Basic
+import Mathlib.Analysis.SpecialFunctions.Exp
+import Mathlib.Analysis.SpecialFunctions.Sigmoid
