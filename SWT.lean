90fae1aa57eb71397c827947b5b7e9ef70290d2f
mortisaac <isaac.mortiboy@gmail.com>
2025-11-11 02:44:16 +0000

    min & max

--- SWT.lean
+++ SWT.lean
@@ -6,6 +6,9 @@ import Mathlib.Topology.ContinuousMap.Compact
 import Mathlib.Topology.DenseEmbedding
 import Mathlib.Algebra.Algebra.Subalgebra.Basic
 -- import Mathlib.Topology.ContinuousMap.Algebra -- Separates Points
+-- import Mathlib.Topology.ContinuousFunction.Lattice
+import Mathlib.Topology.ContinuousMap.Lattice
+import Mathlib.Topology.Order.Basic
 import Mathlib.Topology.Instances.Real.Lemmas
 import Mathlib.Topology.ContinuousMap.Weierstrass
 import Mathlib.Data.Real.Basic
@@ -14,6 +17,23 @@ variable {Ω : Type*} [TopologicalSpace Ω] [CompactSpace Ω]
 variable {F : Subalgebra ℝ C(Ω, ℝ)}
 variable {A : F.topologicalClosure}
 
+-- set_option diagnostics true
+
+/- Proof that a Subalgebra is topologically closed under pointwise absolute value -/
+lemma SubAlgClosureUnderAbs (F : Subalgebra ℝ C(Ω, ℝ)) (f : F.topologicalClosure) : |(f : C(Ω, ℝ))| ∈ F.topologicalClosure := by
+  sorry
+
+/- Proof that a Subalgebra is topologically closed under pointwise maximum -/
+lemma SubAlgClosureUnderMax (F : Subalgebra ℝ C(Ω, ℝ)) (f g : F.topologicalClosure) : ↑f ⊔ ↑g ∈ F.topologicalClosure := by
+  sorry
+
+/- Proof that a Subalgebra is topologically closed under pointwise minimum -/
+lemma SubAlgClosureUnderMin (F : Subalgebra ℝ C(Ω, ℝ)) (f g : F.topologicalClosure) : ↑f ⊓ ↑g ∈ F.topologicalClosure := by
+  sorry
+
+-- /- Definition of a non-zero constant function -/
+-- def NonZeroConst {α β : Type*} (A : Set (α → β)) : Prop :=
+--   ∀ {x y : α}, x ≠ y → ∃ f ∈ A, (f x ≠ f y)
 
 /- Definition of a Set of functions separating points -/
 def SetSeparatesPoints {α β : Type*} (A : Set (α → β)) : Prop :=
@@ -23,7 +43,7 @@ def SetSeparatesPoints {α β : Type*} (A : Set (α → β)) : Prop :=
 abbrev SeparatesPoints (F : Subalgebra ℝ C(Ω, ℝ)) : Prop :=
   SetSeparatesPoints ((fun f : C(Ω, ℝ) ↦ (f : Ω → ℝ)) '' ↑F)
 
-/- Proof that the Topological Closure that separates points, also separates points -/
+/- Proof that if the Subalgebra separates points, then so does its Topological Closure -/
 lemma SubAlgClosureSeparatesPoints (F : Subalgebra ℝ C(Ω, ℝ)) (hF: F.SeparatesPoints) : F.topologicalClosure.SeparatesPoints := by
   sorry
 
